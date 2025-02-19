ba443208503b3650fc5a45ae373739abf79f97e0
Pete Gadomski <pete.gadomski@gmail.com>
2025-02-19 15:22:05 -0500

    --wip-- [skip ci]

--- src/duckdb.rs
+++ src/duckdb.rs
@@ -69,6 +69,48 @@ impl DuckdbClient {
         Ok(dict)
     }
 
+    #[pyo3(signature = (href, *, intersects=None, ids=None, collections=None, limit=None, bbox=None, datetime=None, include=None, exclude=None, sortby=None, filter=None, query=None, **kwargs))]
+    fn search_to_arrow<'py>(
+        &self,
+        py: Python<'py>,
+        href: String,
+        intersects: Option<StringOrDict>,
+        ids: Option<StringOrList>,
+        collections: Option<StringOrList>,
+        limit: Option<u64>,
+        bbox: Option<Vec<f64>>,
+        datetime: Option<String>,
+        include: Option<StringOrList>,
+        exclude: Option<StringOrList>,
+        sortby: Option<StringOrList>,
+        filter: Option<StringOrDict>,
+        query: Option<Bound<'py, PyDict>>,
+        kwargs: Option<Bound<'py, PyDict>>,
+    ) -> Result<Bound<'py, PyObject>> {
+        let search = stac_api::python::search(
+            intersects,
+            ids,
+            collections,
+            limit,
+            bbox,
+            datetime,
+            include,
+            exclude,
+            sortby,
+            filter,
+            query,
+            kwargs,
+        )?;
+        let record_batches = {
+            let client = self
+                .0
+                .lock()
+                .map_err(|err| PyException::new_err(err.to_string()))?;
+            client.search_to_arrow(&href, search)?
+        };
+        todo!()
+    }
+
     fn get_collections<'py>(&self, py: Python<'py>, href: String) -> Result<Bound<'py, PyList>> {
         let client = self
             .0
