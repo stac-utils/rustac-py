56bf3737282cd454ff00f4ca5bbf22aa50ef2064
Pete Gadomski <pete.gadomski@gmail.com>
2025-02-24 13:35:52 -0700

    feat: add example notebook

--- src/arrow.rs
+++ src/arrow.rs
@@ -1,8 +1,23 @@
+use crate::{Error, Json, Result};
+use geoarrow::table::Table;
 use pyo3::prelude::*;
 use pyo3_arrow::PyTable;
 
 #[pyfunction]
-pub fn from_arrow(table: Bound<PyTable>) -> () {
-    dbg!(table);
-    todo!()
+pub fn from_arrow(py: Python<'_>, table: PyTable) -> PyResult<Bound<PyAny>> {
+    let (record_batches, mut schema) = table.into_inner();
+    let record_batches = record_batches
+        .into_iter()
+        .map(|record_batch| {
+            let record_batch = stac::geoarrow::with_native_geometry(record_batch, "geometry")?;
+            Ok(record_batch)
+        })
+        .collect::<Result<Vec<_>>>()?;
+    if !record_batches.is_empty() {
+        schema = record_batches[0].schema();
+    }
+    let table = Table::try_new(record_batches, schema).map_err(Error::from)?;
+    let item_collection = stac::geoarrow::from_table(table).map_err(Error::from)?;
+    let item_collection = Json(item_collection).into_pyobject(py)?;
+    Ok(item_collection)
 }
