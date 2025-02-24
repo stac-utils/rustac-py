6d9e1170932fa723fc9cd28d6889e6e22b12d0df
Pete Gadomski <pete.gadomski@gmail.com>
2025-02-24 06:48:05 -0700

    fix: no ref

--- src/arrow.rs
+++ src/arrow.rs
@@ -2,7 +2,7 @@ use pyo3::prelude::*;
 use pyo3_arrow::PyTable;
 
 #[pyfunction]
-pub fn from_arrow(table: &Bound<PyTable>) -> () {
+pub fn from_arrow(table: Bound<PyTable>) -> () {
     dbg!(table);
     todo!()
 }
