3b8ea0c48ebccac4a18824a7e26b5b21900db14f
Pete Gadomski <pete.gadomski@gmail.com>
2025-03-10 07:38:48 -0600

    fix: remove test

--- tests/test_migrate.py
+++ tests/test_migrate.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from typing import Any
 
 import stacrs
@@ -6,11 +5,4 @@ import stacrs
 
 def test_migrate(item: dict[str, Any]) -> None:
     item = stacrs.migrate(item, version="1.1.0")
-    assert item["stac_version"] == "1.1.0"
-
-
-def test_migrate_href(spec_examples: Path) -> None:
-    item = stacrs.migrate_href(
-        str(spec_examples / "v1.0.0" / "simple-item.json"), version="1.1.0"
-    )
     assert item["stac_version"] == "1.1.0"
