f430fdddfb9c78787d27a1204ec6752cbadb7fea
Pete Gadomski <pete.gadomski@gmail.com>
2026-03-10 12:35:05 -0600

    fix: more permissive geoparquet reading (#253)
    Requires a lot of patches to arrow v58 in upstreams. Closes #251

--- tests/test_arrow.py
+++ tests/test_arrow.py
@@ -11,3 +11,11 @@ def test_to_arrow(item: dict[str, Any]) -> None:
     table = rustac.to_arrow([item])
     data_frame = GeoDataFrame.from_arrow(table)
     assert len(data_frame) == 1
+
+
+def test_from_arrow(item: dict[str, Any]) -> None:
+    from arro3.core import Table
+
+    data_frame = GeoDataFrame.from_features([item])
+    item_collection = rustac.from_arrow(Table.from_arrow(data_frame.to_arrow()))
+    assert len(item_collection["features"]) == 1
